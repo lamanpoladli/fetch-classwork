53792f155bace014980dc019cab5fec7d3559287
natiqmammadovvv <natiq2710@gmail.com>
2023-04-28 13:20:45 +0400

    bigtask

--- script.js
+++ script.js
@@ -1,119 +1,91 @@
-const API_URL = 'https://restcountries.com/v3.1/all';
-const search = document.querySelector("#search");
+const API_URL = 'https://northwind.vercel.app/api/suppliers';
 (async function getData() {
-    const row = document.querySelector(".row");
-    const select = document.querySelector("select");
+        const tableBody = document.querySelector(".tableBody");
 
-    try{
-        const res = await fetch(API_URL)
-        const data = await res.json()
-
-        data.forEach(element => {
-            let newCard = createCard(element);
-            row.append(newCard);
-        });
     
-        select.addEventListener("change", function () {
-            row.innerHTML = "";
+        try{
+            const res = await fetch(API_URL)
+            const data = await res.json()
     
-            if (this.value == "id") {
-                data.sort((a, b) => a.population - b.population)
-                data.forEach(elem => {
-                    row.append(createCard(elem))
-                })
-            } else if (this.value == "username") {
-                data.sort((a, b) => a.name.common.localeCompare(b.name.common))
-                data.forEach(elem => {
-                    row.append(createCard(elem))
-                })
-            } else {
-                row.innerHTML = "adam ol"
-            }
-        })
-        search.addEventListener("keyup",(e)=>{
-                row.innerHTML = "";
-                let filteredData = data.filter((user)=>user.name.common.trim().toLowerCase().includes(e.target.value.trim().toLowerCase()))
-                filteredData.forEach((user)=>{
-                    let newCard = createCard(user);
-                    row.append(newCard);
-                })
-            })
+            data.forEach(elem => {
+                let newRow = createRow(elem);
+                tableBody.appendChild(newRow);
+            });
         
+        }catch(err){
+            console.log(err)
+        }
+    })();
 
 
-    }catch(err){
-        console.log(err)
-    }
 
+function createRow(elem) {
+    let row = document.createElement("tr")
+
+    let id = document.createElement("td")
+    id.innerText = elem.id;
+
+    let name = document.createElement("td")
+    name.innerText = elem?.contactName;
+
+    let title = document.createElement("td")
+    title.innerText = elem?.contactTitle;
+
+    let country = document.createElement("td")
+    country.innerText = elem?.address?.country;
     
+    let btnTd = document.createElement("td")
+    let btn = document.createElement("a");
+    btn.setAttribute("role","button");
+    btn.setAttribute("href","edit.html");
+    let btnRight = document.createElement("button")
 
-})();
-
-// fetch(API_URL)
-//     .then(res => res.json())
-//     .then(data =>{
-//         console.log(data)
-//         data.forEach(element => {
-//             let newCard = createCard(element);
-//             row.append(newCard);
-//         });
-
-//         select.addEventListener("change", function(){
-//             row.innerHTML = "";
-
-//             if(this.value == "id"){
-//                 data.sort((a, b) => a.id - b.id)
-//                 data.forEach(elem => {
-//                     row.append(createCard(elem))
-//                 })
-//             }else if(this.value == "username"){
-//                 data.sort((a, b)=> a.username.localeCompare(b.username))
-//                 data.forEach(elem => {
-//                     row.append(createCard(elem))
-//                 })
-//             }else{
-//                 row.innerHTML = "adam ol"
-//             }
-
-
-//         })
-//     })
+    btn.classList.add("btn-primary")
+    btn.style.backgroundColor="rgb(158, 158, 87)"
+    btn.style.borderRadius="5px"
+    btn.style.marginRight="10px"
+    btn.style.color="white"
+    btn.style.width="30px"
+    btn.style.height="30px"
+    btn.style.padding="7px"
+    
+    let btnIcon = document.createElement("i");
+    btn.classList.add("fa-solid");
+    btn.classList.add("fa-pencil");
+    btn.append(btnIcon)
+    btnRight.classList.add("btn-secondary")
+    btnRight.style.backgroundColor="rgb(228, 81, 81)"
+    btnRight.style.borderRadius="5px"
+    btnRight.style.marginRight="10px"
+    btnRight.style.width="30px"
+    btnRight.style.height="30px"
+    
+    let btnRightIcon = document.createElement("i");
 
 
+    btnRightIcon.classList.add("fa-trash-can");
+    btnRightIcon.classList.add("fa-solid");
+  
+    btnRight.append(btnRightIcon);
 
+    const deleteCategoryByID = async (id)=>{
+        await fetch(`${API_URL}/${id}`,{
+            method: "DELETE",
+        })
+    };
+    btnRight.addEventListener("click",(e)=>{
+        if(!confirm("Are you sure you want to delete?")) return;
+        deleteCategoryByID(elem.id);
+        e.target.parentElement.parentElement.remove();
+    })
+        
+    
+    btnTd.append(btn,btnRight)
 
+    row.append(id,name, title, country, btnTd)
 
-function createCard(user) {
-    let col = document.createElement("div");
-    col.classList.add("col-3");
-    col.classList.add("mb-3");
+    return row;
 
-    let card = document.createElement("div");
-    card.classList.add("card");
-
-    let image = document.createElement("img");
-    image.setAttribute("src", user.flags.png);
-
-    let cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-
-    let title = document.createElement("h5");
-    title.classList.add("card-title");
-    title.innerText = user.name.common;
-
-    let companyName = document.createElement("p");
-    companyName.classList.add("card-list");
-    companyName.innerText = user.region;
-
-    let email = document.createElement("a");
-    email.setAttribute("href", `${user.maps.googleMaps}`);
-    email.innerText = "map";
-
-    cardBody.append(title, companyName,email);
-    card.append(image, cardBody);
-    col.append(card);
-
-    return col;
 
 
 }
